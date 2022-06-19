6508c634dcadae940a8ff1f106c9e070b8bdc4ca
Francisco-2103 <al270032@edu.uaa.mx>
2022-06-19 00:31:04 -0500

    modificacion de componentes

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { ContactoComponent } from './consultas/contacto.component';
+import { ContactoComponent } from './contacto/contacto.component';
 import { LoginComponent } from './login/login.component';
 import { AyudaComponent } from './ayuda/ayuda.component';
 import { AboutComponent } from './about/about.component';
