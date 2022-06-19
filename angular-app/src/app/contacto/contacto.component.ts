b0edfb96f4f198cc766c700304d4b8e11b0c02ed
Isaac-G-O <nevekdlh@gmail.com>
2022-06-19 12:03:53 -0500

    contacto

--- angular-app/src/app/contacto/contacto.component.ts
+++ angular-app/src/app/contacto/contacto.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CorreoService } from '../correo.service';
 import * as Notiflix from 'notiflix';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
@@ -8,27 +9,28 @@ import * as Notiflix from 'notiflix';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent implements OnInit {
+  
+  datos!:FormGroup;
 
   constructor(private correo: CorreoService) { 
-    //AQUI VAN LAS COSAS DEL FORMGROUP
-    /*
-    aunto
-    mensaje
-    */
+    this.datos = new FormGroup ({
+      asunto: new FormControl('',Validators.required),
+      mensaje: new FormControl('',Validators.required)
+    })
   }
 
   enviar() {
-    // Notiflix.Loading.standard('loading...');
-    // let data = {
-    //   emai:'TU CORREO AQUI si no el mio pacoo12306@gmail.com',
-    //   asunto:this.datos.value.asunto,
-    //   mensaje:this.datos.value.mensaje
-    // };
+    Notiflix.Loading.standard('loading...');
+    let data = {
+      emai:'pacoo12306@gmail.com',
+      asunto:this.datos.value.asunto,
+      mensaje:this.datos.value.mensaje
+    };
 
-    // this.correo.sendEmail(data).subscribe((res:any) =>{
-    //   console.log(res);
-    //   Notiflix.Loading.remove();
-    // })
+    this.correo.sendEmail(data).subscribe((res:any) =>{
+      console.log(res);
+      Notiflix.Loading.remove();
+    });
   }
 
   ngOnInit(): void {
